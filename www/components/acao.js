d824891bc2f75c8e49321b03f154ee8ea7f3ed41
VicMartins <vic.martins0907@gmail.com>
2019-10-02 03:32:26 +0900

    criando comunicação com servidor

--- www/components/acao.js
+++ www/components/acao.js
@@ -2,3 +2,26 @@
 $(document).on('click',"#listarPizza",function(){
     $(location).attr("href","listarPizza.html");
 });
+
+$(document).on('click','#salvar',function(){
+    var parametros ={
+     "sabor":$("$sabor").val(),
+     "valor":$("#valor").val()
+    };
+
+    $.ajax({
+        type:"post",//como vou enviar os dados ao servidor
+        url:"https://stey-appropriations.000webhostapp.com/cadastra.php",//para onde vou enviar
+        data:parametros,// o que eu vou enviar
+        //caso dê certo esse código é executado
+        success: function(data){
+            navigator.notification.alert(data);
+            $("$sabor").val(""),
+            $("#valor").val("")
+        },
+        //caso dê erro esse código é executado
+        error: function(data){
+            navigator.notification.alert("Erro ao cadastrar!");
+        }
+    });
+});
